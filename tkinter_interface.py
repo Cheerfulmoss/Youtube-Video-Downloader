33658e23f608cbb327647ddff0a3c9d9ee751ad8
Cheerfulmoss <41566305+Cheerfulmoss@users.noreply.github.com>
2022-06-26 11:42:23 +1000

    Add files via upload

---
+++ tkinter_interface.py
@@ -0,0 +1,144 @@
+from tkinter import ttk, Tk, BooleanVar
+from video_downloader_class import VideoDownloader
+import json
+import threading
+import ctypes
+
+startup_directory = 0
+startup_url = 0
+settings = "preferences.json"
+threads = 5
+
+
+def check_preferences():
+    download_folder = folder_directory.get()
+    with open(settings, "r") as file:
+        preferences = json.load(file)
+        preferences["download_folder"] = download_folder
+        file = open(settings, "w")
+        json.dump(preferences, file)
+        file.close()
+    return download_folder
+
+
+def fast_download():
+    warning = ctypes.windll.user32.MessageBoxW(0, "Fast Download is at maximum 720p, unlike Download which can"
+                                                  " go up to 4K. "
+                                                  "Fast download does not use FFMPEG.\n"
+                                                  "Are you sure you want to start the download?", "WARNING", 1)
+    if warning == 2:
+        return
+
+    if thread_count_state.get():
+        num_threads = threads
+    else:
+        num_threads = 5
+    download_folder = check_preferences()
+    video_url = video_url_box.get()
+    video_url_box.delete(0, "end")
+    root.title("DO NOT CLOSE")
+    VideoDownloader(download_folder).\
+        threaded_download(video_url=video_url, audio_set=audio_state.get(), num_threads=num_threads)
+    video_url_box.delete(0, "end")
+    root.title("Downloader")
+
+
+def thread_start():
+    warning = ctypes.windll.user32.MessageBoxW(0, "Are you sure you want to start the download", "WARNING", 1)
+    if warning == 2:
+        return
+    t1 = threading.Thread(target=button_download)
+    t1.start()
+
+
+def button_download():
+    download_folder = check_preferences()
+    video_url = video_url_box.get()
+    video_url_box.delete(0, "end")
+    root.title("DO NOT CLOSE")
+    VideoDownloader(download_folder).download(video_url=video_url, audio_set=audio_state.get(),
+                                              high_quality_set=ffmpeg_state.get())
+    video_url_box.delete(0, "end")
+    root.title("Downloader")
+
+
+def increment_threads():
+    global threads
+    threads += 1
+    thread_label.configure(text=f"Thread count: {threads}")
+
+
+def decrement_threads():
+    global threads
+    if threads == 1:
+        threads += 0
+    else:
+        threads -= 1
+    thread_label.configure(text=f"Thread count: {threads}")
+
+
+def exit():
+    root.destroy()
+
+
+def temporary_text_directory(e):
+    global startup_directory
+    startup_directory += 1
+    if startup_directory == 1:
+        folder_directory.delete(0, "end")
+
+
+def temporary_text_url(e):
+    global startup_url
+    startup_url += 1
+    if startup_url == 1:
+        video_url_box.delete(0, "end")
+
+
+root = Tk()
+root.geometry("290x150")
+root.resizable(False, False)
+root.title("Downloader")
+frm = ttk.Frame(root, padding=10)
+frm.pack(fill="both", expand=True)
+download_button = ttk.Button(frm, text="Download", command=thread_start)
+download_button.place(y=0, x=0)
+fast_download_button = ttk.Button(frm, text="Fast Download", command=fast_download)
+fast_download_button.place(y=30, x=0)
+
+increase_thread_count = ttk.Button(frm, text="-", command=decrement_threads)
+increase_thread_count.place(y=30, x=100)
+thread_label = ttk.Label(frm, text=f"Thread count: {threads}")
+thread_label.place(y=0, x=150)
+decrease_thread_count = ttk.Button(frm, text="+", command=increment_threads)
+decrease_thread_count.place(y=30, x=200)
+
+with open(settings, "r") as file:
+    preferences = json.load(file)
+    directory = preferences["download_folder"]
+
+folder_directory = ttk.Entry(root)
+folder_directory.insert(0, directory)
+folder_directory.place(y=90, x=135)
+if directory == "Download Directory":
+    folder_directory.bind("<FocusIn>", temporary_text_directory)
+
+video_url_box = ttk.Entry(root)
+video_url_box.insert(0, "Video URL")
+video_url_box.place(y=70, x=135)
+video_url_box.bind("<FocusIn>", temporary_text_url)
+
+audio_state = BooleanVar()
+audio_only_button = ttk.Checkbutton(frm, text="Audio only", variable=audio_state)
+audio_only_button.place(y=90, x=0)
+ffmpeg_state = BooleanVar()
+ffmpeg_button = ttk.Checkbutton(frm, text="FFMPEG", variable=ffmpeg_state)
+ffmpeg_button.place(y=110, x=0)
+thread_count_state = BooleanVar()
+change_thread_count = ttk.Checkbutton(frm, text="Threads", variable=thread_count_state)
+change_thread_count.place(y=110, x=80)
+
+quit_button = ttk.Button(frm, text="Quit", command=exit)
+quit_button.place(y=60, x=0)
+
+root.mainloop()
---
+++ video_downloader_class.py
@@ -0,0 +1,107 @@
+from pytube import Playlist
+from pytube import YouTube
+import subprocess
+import os
+import threading
+import math
+
+from pytube.exceptions import VideoUnavailable
+
+
+class VideoDownloader():
+    def __init__(self, music_folder):
+        self.current_folder = os.getcwd()
+        self.music_folder = music_folder
+
+    def audio_only_download(self, video_url, download_path, for_merger=False):
+        # 140 is 128kbps, 130 is 48kbps
+        audio_bitrates_mp4 = [140, 139]
+
+        audio = YouTube(video_url).streams
+        for bitrate in audio_bitrates_mp4:
+            if bitrate in audio.itag_index.keys():
+                if for_merger:
+                    audio.get_by_itag(bitrate). \
+                        download(output_path=download_path, filename="audio_input.mp4")
+                else:
+                    audio.get_by_itag(bitrate).download(output_path=download_path)
+                return f"Audio bitrate preset: {bitrate}"
+
+    def video_only_download(self, video_url):
+        # Ordered from highest quality (2160p) to lowest quality (144p).
+        # When two values are used for a resolution it means they are using a different codex.
+        # values: (401, 2160p), (400, 1440p), (299/399, 1080p), (298/398, 720p),
+        # (135/397, 480p), (134/396, 360p), (133/395, 240p), (160/394, 144p).
+        video_resolutions = [401, 400, 299, 399, 298, 398, 135, 397, 134, 396, 133, 395, 160, 394]
+
+        video = YouTube(video_url).streams
+        for resolution in video_resolutions:
+            if resolution in video.itag_index.keys():
+                video.get_by_itag(resolution).\
+                    download(output_path=self.current_folder, filename="video_input.mp4")
+                return f"Video resolution preset: {resolution}"
+
+    def download_video(self, video_url, audio_only, high_quality):
+        try:
+            YouTube(video_url)
+        except VideoUnavailable:
+            print(f"Video {video_url} is unavailable, skipping")
+            return
+        if audio_only:
+            self.audio_only_download(video_url, download_path=self.music_folder)
+        else:
+            if high_quality:
+                video_name = str(YouTube(video_url).title)
+                filename = ""
+                disallowed_character_set = ["<", ">", ":", '"', "/", "\\", "|", "?", "*"]
+                for letter in video_name:
+                    if letter not in disallowed_character_set:
+                        filename = filename + letter
+                bitrate = self.audio_only_download(video_url, download_path=self.current_folder, for_merger=True)
+                resolution = self.video_only_download(video_url)
+                print(f"{bitrate}\n{resolution}")
+                cmd = f"ffmpeg -i video_input.mp4 -i audio_input.mp4 -c:v copy -c:a aac output.mp4"
+                subprocess.call(cmd, shell=False)
+                os.remove("video_input.mp4")
+                os.remove("audio_input.mp4")
+                os.replace(self.current_folder + "\\output.mp4", self.music_folder + f"\\output.mp4")
+                os.rename(self.music_folder + f"\\output.mp4", self.music_folder + f"\\{filename}.mp4")
+            else:
+                # This is generally of lower quality or sometimes non-existent.
+                # (22, 720p), (18, 360p).
+                combined_audio_video = [22, 18]
+                video = YouTube(video_url).streams
+                for identifier in combined_audio_video:
+                    if identifier in video.itag_index.keys():
+                        video.get_by_itag(identifier). \
+                            download(output_path=self.music_folder)
+                        print(f"Combined audio and video tag: {identifier}")
+                        return
+
+    def download_playlist(self, video_url, audio_only, high_quality):
+        for video_url in Playlist(video_url):
+            self.download_video(video_url, audio_only=audio_only, high_quality=high_quality)
+
+    def download(self, video_url, audio_set, high_quality_set):
+        if "list=" in video_url:
+            self.download_playlist(video_url, audio_only=audio_set, high_quality=high_quality_set)
+        else:
+            self.download_video(video_url, audio_only=audio_set, high_quality=high_quality_set)
+
+    def list_input(self, video_list, audio_only):
+        for video_url in video_list:
+            self.download_video(video_url, audio_only=audio_only, high_quality=False)
+
+    def threaded_download(self, video_url, audio_set, num_threads=5):
+        if "list=" in video_url:
+            playlist_object = Playlist(video_url)
+            playlist_list = list(playlist_object)
+            split_list = [playlist_list[i::num_threads] for i in range(num_threads)]
+            threads = []
+            print(split_list)
+            for index in range(num_threads):
+                t = threading.Thread(target=self.list_input, args=(split_list[index], audio_set))
+                t.start()
+                threads.append(t)
+        else:
+            self.download(video_url=video_url, audio_set=audio_set, high_quality_set=False)
