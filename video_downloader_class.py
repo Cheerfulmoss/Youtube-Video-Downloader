395195f521b4760c1532d7a22a070bbb45d6d693
Cheerfulmoss <41566305+Cheerfulmoss@users.noreply.github.com>
2022-08-06 21:09:47 +1000

    Update video_downloader_class.py

--- video_downloader_class.py
+++ video_downloader_class.py
@@ -12,32 +12,25 @@ class VideoDownloader:
         self.current_folder = getcwd()
         self.music_folder = music_folder
 
-    def audio_only_download(self, video_url, download_path, for_merger=False):
-        audio = YouTube(video_url).streams.filter(file_extension="mp4", type="audio", only_audio=True).order_by("abr")[-1]
+    def audio_only_download(self, video_object, download_path, for_merger=False):
+        audio = video_object.streams.filter(file_extension="mp4", type="audio", only_audio=True).order_by("abr")[-1]
         if for_merger:
             audio.download(output_path=download_path, filename="audio_input.mp4")
         else:
             audio.download(output_path=download_path)
         return audio
 
-    def video_only_download(self, video_url):
-        video = YouTube(video_url).streams.filter(file_extension="mp4", type="video", only_video=True).order_by("resolution")[-1]
+    def video_only_download(self, video_object):
+        video = video_object.streams.filter(file_extension="mp4", type="video", only_video=True).order_by("resolution")[-1]
         video.download(output_path=self.current_folder, filename="video_input.mp4")
         return video
 
-    def download_video(self, video_url, audio_only, high_quality):
-        try:
-            YouTube(video_url)
-        except VideoUnavailable:
-            print(f"Video {video_url} is unavailable, skipping")
-            return
+    def download_video(self, video_object, audio_only, high_quality):
         if audio_only:
-            self.audio_only_download(video_url, download_path=self.music_folder)
-            print(f"{YouTube(video_url).title} downloaded: audio only")
+            self.audio_only_download(video_object, download_path=self.music_folder)
+            print(f"{video_object.title} downloaded: audio only")
         else:
             if high_quality:
-                n = 0
-                video_object = YouTube(video_url)
                 video_name = str(video_object.title)
                 video_author = str(video_object.author)
                 video_year = str(video_object.publish_date)
@@ -48,8 +41,8 @@ class VideoDownloader:
                     if letter not in disallowed_character_set:
                         filename = filename + letter
                 threads = list()
-                threads.append(Thread(target=self.audio_only_download, args=(video_url, self.current_folder, True)))
-                threads.append(Thread(target=self.video_only_download, args=(video_url,)))
+                threads.append(Thread(target=self.audio_only_download, args=(video_object, self.current_folder, True)))
+                threads.append(Thread(target=self.video_only_download, args=(video_object,)))
                 for thread in threads:
                     thread.start()
                 for thread in threads:
@@ -60,6 +53,7 @@ class VideoDownloader:
                 call(cmd, shell=False, creationflags=0x00000008)
                 remove("video_input.mp4")
                 remove("audio_input.mp4")
+                n = 0
                 while os.path.exists(f"{self.music_folder}\\output-{n}.mp4"):
                     n += 1
                 replace(self.current_folder + "\\output.mp4", self.music_folder + f"\\output-{n}.mp4")
@@ -72,23 +66,39 @@ class VideoDownloader:
                     rename(self.music_folder + f"\\output-{n}.mp4", self.music_folder + f"\\{filename}.mp4")
                 print(f"{filename} downloaded: ffmpeg")
             else:
-                YouTube(video_url).streams.filter(file_extension="mp4", progressive=True).order_by("resolution")[-1].download(output_path=self.music_folder)
-                print(f"{YouTube(video_url).title} downloaded: progressive")
+                video_object.streams.filter(file_extension="mp4", progressive=True).order_by("resolution")[-1].download(output_path=self.music_folder)
+                print(f"{video_object.title} downloaded: progressive")
 
-    def download_playlist(self, video_url, audio_only, high_quality):
-        for video_url in Playlist(video_url):
-            self.download_video(video_url, audio_only=audio_only, high_quality=high_quality)
+    def download_playlist(self, url, audio_only, high_quality):
+        for video_url in Playlist(url):
+            try:
+                video_object = YouTube(video_url)
+            except VideoUnavailable:
+                print(f"Playlist {video_url} unavailable")
+                continue
+            self.download_video(video_object, audio_only=audio_only, high_quality=high_quality)
 
     def auto_download(self, video_url, audio_set, high_quality_set):
         if "list=" in video_url:
             self.download_playlist(video_url, audio_only=audio_set, high_quality=high_quality_set)
+            print(f"Finished downloading playlist: {Playlist(video_url).title}")
         else:
-            self.download_video(video_url, audio_only=audio_set, high_quality=high_quality_set)
-        print(f"Completed, all videos downloaded")
+            try:
+                video_object = YouTube(video_url)
+            except VideoUnavailable:
+                print(f"Video {video_url} unavailable")
+                return
+            self.download_video(video_object, audio_only=audio_set, high_quality=high_quality_set)
+            print(f"Finished downloading video: {YouTube(video_url).title}")
 
     def list_input(self, video_list, audio_only):
         for video_url in video_list:
-            self.download_video(video_url, audio_only=audio_only, high_quality=False)
+            try:
+                video_object = YouTube(video_url)
+            except VideoUnavailable:
+                print(f"Video {video_url} unavailable")
+                continue
+            self.download_video(video_object, audio_only=audio_only, high_quality=False)
 
     def threaded_download(self, video_url, audio_set, num_threads=5):
         if "list=" in video_url:
